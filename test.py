8169f600c4e3ae6dff4b90910261c552b65d45e7
omarhabashi <oh0785v@gre.ac.uk>
2024-04-01 22:35:11 +0100

    testing of functions

--- test.py
+++ test.py
@@ -1,43 +1,47 @@
-from final_product import Data_Processor
-from final_product import Datapoint_Extraction
+
+from final_product import Datapoint_Extraction,Data_Processor
 import pytest
 from api_storer import api_key
 import os
-class Test_De:
-    def test_location_finder(self):
-        extractor = Datapoint_Extraction(api_key)
-        location_id = extractor.location_finder('London')
-        assert location_id == '352409'
+import pandas as pd
 
-    def test_forcast_finder(self):
-        extractor = Datapoint_Extraction(api_key)
-        forecast_data = extractor.forcast_finder(['London', 'Manchester'])
-        assert len(forecast_data) > 0
-        assert 'location' in forecast_data.columns
-        assert 'Date' in forecast_data.columns
+api_key=api_key
 
-class Test_DP:
-    class Test_DP:
-        def test_column_deleter:
-            temp_dir=tmpdir.mkdir("test_data")
-            csv_filename="test_data.csv"
-            csv_filepath=os.path.join(temp_dir,csv_filename)
-            test_data={
-                "A":[1,2,3],
-                "B":[4,5,6],
-                "C":[7,8,9]
-            }
-            df=pd.Dataframe(test_data)
-            df.to_csv(csv_filepath,index=False)
-            deleted_df=col
+def Datapoint_instance():
+    return Datapoint_Extraction(api_key)
+
+def Dataprocessor_instance():
+    return Data_Processor()
+
+def test_location_finder(Datapoint_instance):
+    assert Datapoint_instance.location_finder('London')='352409'
+
+def test_forecast_finder(Datapoint_instance):
+    locations=["London","Manchester"]
+    df=Datapoint_instance.forcast_finder(locations)
+    assert isinstance(df,pd.DataFrame)
+
+def test_csv_saver(DataPoint_instance):
+    df=pd.DataFrame({"A":[1,2,3],"B":[4,5,6]})
+    file_name="test.csv"
+    DataPoint_instance.csv_saver(df,file_name)
+    assert os.path.isfile(file_name)
+    os.remove(file_name)
 
 
+def test_column_deleter(Dataprocessor_instance):
+    csv_filename="test.csv"
+    column_name="B"
+    df=Dataprocessor_instance.column_deleter(csv_filename,column_name)
+    assert isinstance(df,pd.DataFrame)
+    assert column_name not in df.columns
 
-def test_weather_mapper:
-        processor = Data_Processor
-        processor.weather_mapper(r'C:\Users\User\OneDrive\Desktop\data1',
-                                    r'C:\Users\User\OneDrive\Desktop\data1',
-                                    'LONDON_AND_NEWCASTLE.csv',
-                                    'modified.csv')
-
+def test_weather_mapper(Dataprocessor_instance):
+    input_directory="input.csv"
+    output_directory="output.csv"
+    csv_filename="input.csv"
+    new_filename="output.csv"
+    df=Dataprocessor_instance.weather_mapper(input_directory,output_directory,csv_filename,new_filename)
+    assert isinstance(df,pd.DataFrame)
+    assert os.path.isfile(os.path.join(output_directory,new_filename))
 
